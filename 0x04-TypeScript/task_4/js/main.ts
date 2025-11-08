aa30cec85117ec236bd4c906a4b1e9f2ef8aa573
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-08 18:09:33 +0200

    Create main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,39 @@
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import { Subjects } from './subjects/Cpp';
+import { Subjects as JavaSubjects } from './subjects/Java';
+import { Subjects as ReactSubjects } from './subjects/React';
+
+// Create and export the subject instances
+export const cpp = new Subjects.Cpp();
+export const java = new JavaSubjects.Java();
+export const react = new ReactSubjects.React();
+
+// Create and export the teacher
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
+};
+
+// For Cpp
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
