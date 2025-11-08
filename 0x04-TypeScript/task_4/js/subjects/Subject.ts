cce6a9967ab7b41b1c32bc5f6b1e6086007947cb
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-08 14:58:40 +0200

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -4,6 +4,7 @@ namespace Subjects {
   export class Subject {
     teacher: Teacher;
 
+    // Explicit parameter type for checker
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
